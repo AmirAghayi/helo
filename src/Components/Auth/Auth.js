6c523a96312d4dd9fd0b53fc5f3a2492488df907
Amir aghayi_chadegani <amiraghayi_chadegani@Amirs-iMac.local>
2019-04-05 19:37:47 -0600

    components-done

---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+class Auth extends Component {
+    
+
+
+render(){
+    return(
+        <div>
+            Auth
+        </div>
+    );
+
+
+}
+
+
+
+} 
+
+
+export default Auth;
